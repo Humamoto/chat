f20ec3f0e1db9f5c23afd0398feb879afc0e04d9
Humamoto <130789594+Humamoto@users.noreply.github.com>
2024-11-28 00:06:29 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ import time
 import re
 import json
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 # Configuração da página
 st.set_page_config(
     page_title="Chat em Tempo Real",
